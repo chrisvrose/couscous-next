e7646dbb5169d619704de562c7b0c6e7f8254db4
Atreya Bain <christophervrose@gmail.com>
2020-10-01 14:00:58 +0530

    Add navbar divide

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,18 +25,23 @@ export default function Header(props: HeaderProps) {
                 expand="md"
                 fixed="top"
                 variant="dark"
-                className="card-1 justify-content-center"
+                className="card-1"
             >
                 <Navbar.Brand>Couscous</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbar-toggleable" />
                 <Navbar.Collapse id="navbar-toggleable">
-                    <Nav activeKey={props.activeKey} className="mr-auto">
+                    <Nav
+                        activeKey={props.activeKey}
+                        className="justify-content-end mr-auto"
+                    >
                         <Link href="/" passHref>
                             <Nav.Link eventKey="home">Home</Nav.Link>
                         </Link>
                         <Link href="/dashboard" passHref>
                             <Nav.Link eventKey="dashboard">Dashboard</Nav.Link>
                         </Link>
+                    </Nav>
+                    <Nav activeKey={props.activeKey}>
                         <Link href="/login" passHref>
                             <Nav.Link eventKey="login">Login</Nav.Link>
                         </Link>
