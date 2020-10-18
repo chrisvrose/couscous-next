f113b869632fffb4de07dd9f4aac1e65ef034321
Atreya Bain <christophervrose@gmail.com>
2020-10-18 21:56:24 +0530

    Add mainPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,30 @@
-import { Container,Row, Col } from 'react-bootstrap';
+import { Button, Col, Container, Jumbotron, Row } from 'react-bootstrap';
 import Header from '../components/Header';
 import styles from '../styles/index.module.css';
 
 export default function Home() {
-  return (
-    <>
-      <Header title="Home" activeKey='home'/>
-      <Container fluid="md">
-        <Row>
-          <Col>Hi!</Col>
-        </Row>
-        <div className={styles.needSpace}></div>
-      </Container>
-    </>
-  )
+    return (
+        <>
+            <Header title="Home" activeKey="home" />
+            <Container fluid="md">
+                <Row>
+                    <Col>
+                        <Jumbotron>
+                            <h1>Couscous</h1>
+                            <p>
+                                The web interface to the distributed FileSystem
+                            </p>
+                            <Button
+                                href="https://github.com/chrisvrose/couscous-next"
+                                variant="outline-primary"
+                            >
+                                Learn more
+                            </Button>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+                <div className={styles.needSpace}></div>
+            </Container>
+        </>
+    );
 }
