2adb8321b91b6ddcb29ce96330830b4ec55768b2
Atreya Bain <christophervrose@gmail.com>
2020-11-10 23:27:48 +0530

    add tables

--- misc/dbinit/init.sql
+++ misc/dbinit/init.sql
@@ -18,3 +18,40 @@ CREATE TABLE atokens (
 
 -- user with password 'password'
 insert into users(name,email,pwd, role) values('Foo Bar', 'foo@bar.baz','$2a$10$/comR4xTKpklPzmhSplXLO3cCOST1M96p2Jvwtxvrl6B5EHeONWtS',True);
+
+
+CREATE TABLE usergroups (
+    gid integer not null auto_increment PRIMARY KEY
+);
+
+CREATE TABLE folder(
+    foid integer not null auto_increment PRIMARY KEY,
+    uid integer not null,
+    gid integer not null,
+    permissions integer not null,
+    parentfoid integer not null,
+    foreign key (uid) references users(uid),
+    foreign key (gid) references usergroups(gid),
+    foreign key (parentfoid) references folder(foid)
+);
+
+CREATE TABLE file(
+    fid integer not null auto_increment PRIMARY KEY,
+    uid integer not null,
+    gid integer not null,
+    permissions integer not null,
+    parentfoid integer not null,
+    foreign key (uid) references users(uid),
+    foreign key (gid) references usergroups(gid),
+    foreign key (parentfoid) references folder(foid)
+);
+
+
+
+CREATE TABLE usersession(
+    uid integer not null,
+    operation integer not null,
+    fid integer not null,
+    foreign key (uid) references users(uid),
+    foreign key (fid) references file(fid)
+);
