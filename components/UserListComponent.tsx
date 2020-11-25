22eb74d65073ed2502e6344c1daf936b85790fa6
Atreya Bain <christophervrose@gmail.com>
2020-11-25 20:01:14 +0530

    add promote users

--- components/UserListComponent.tsx
+++ components/UserListComponent.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Accordion, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Accordion, Button, Card, Spinner } from 'react-bootstrap';
 import { UserElement } from '../lib/types/Users';
 
 interface UserListProps {
@@ -7,6 +7,30 @@ interface UserListProps {
     doRefresh: () => Promise<any>;
 }
 export default function UserListComponent({ data, doRefresh }: UserListProps) {
+    const [updating, setUpdating] = useState(false);
+    const handleSubmit = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        if (!updating) {
+            setUpdating(true);
+            fetch('/api/user/setRole', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ uid: data.uid, role: 1 - data.role }),
+            })
+                .then(result => {
+                    return doRefresh();
+                })
+                .then(res => {
+                    setUpdating(false);
+                })
+                .catch(err => {
+                    setUpdating(false);
+                });
+        }
+    };
+
+    console.log(data.role);
     return (
         <Card>
             <Accordion.Toggle
@@ -17,7 +41,24 @@ export default function UserListComponent({ data, doRefresh }: UserListProps) {
                 {data.email}
             </Accordion.Toggle>
             <Accordion.Collapse eventKey={data.uid.toString()}>
-                <Card.Body>{data.role ? 'Admin' : 'User'}</Card.Body>
+                <Card.Body>
+                    <div>Name - {data.name}</div>
+                    <div>Email - {data.email}</div>
+                    <div>Role - {data.role ? 'Admin' : 'User'}</div>
+                    <h5 className="spacer-top-margin">Change options</h5>
+                    <Button variant="outline-warning" onClick={handleSubmit}>
+                        {updating && (
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                        )}
+                        Make {data.role ? 'User' : 'Admin'}
+                    </Button>
+                </Card.Body>
             </Accordion.Collapse>
         </Card>
     );
