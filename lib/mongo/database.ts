c6800b263c8f1af94343a9a330f3a49273d3cc55
Atreya Bain <christophervrose@gmail.com>
2020-12-05 08:29:33 +0530

    Fix database connectivity issues

--- lib/mongo/database.ts
+++ lib/mongo/database.ts
@@ -2,15 +2,28 @@ import { GridFSBucket, MongoClient } from 'mongodb';
 
 export const mongos = new MongoClient('mongodb://mongosdb:27017/test', {
     maxPoolSize: 15,
+    useUnifiedTopology: true,
 });
+
+let mongoclient: MongoClient = null;
 
 /**
  * Connect to mongodb and get db
  * @returns database
  */
 export async function mdb() {
-    const conn = await mongos.connect();
-    return conn.db('test');
+    // if (!mongos.isConnected) {
+    //     console.log('I>connecting');
+    //     mongoclient = await mongos.connect();
+    //     return m
+    // }
+    if (!mongoclient || !mongoclient.isConnected) {
+        mongoclient = await mongos.connect();
+    }
+
+    return mongoclient.db('test');
+    // const conn = await mongos.connect();
+    // return conn.db('test');
 }
 
 export async function getBucket() {
